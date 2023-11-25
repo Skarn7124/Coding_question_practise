bb37826a23a88c88ec85f507dcfaeff2bdd5bb6e
Shashwat Khare <97393095+Skarn7124@users.noreply.github.com>
2023-11-26 01:26:04 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,34 @@
+/*
+You are given an integer array nums sorted in non-decreasing order.
+
+Build and return an integer array result with the same length as nums such that result[i] is equal to the summation of absolute differences between nums[i] and all the other elements in the array.
+
+In other words, result[i] is equal to sum(|nums[i]-nums[j]|) where 0 <= j < nums.length and j != i (0-indexed).
+*/
+
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        vector<int> prefixSum(n), suffixSum(n);
+        
+        // Calculate and initialize the prefix sums & suffixSum array
+        prefixSum[0] = nums[0];
+        suffixSum[n - 1] = nums[n - 1];
+        
+        // Calculate the prefix sums & suffixSum array in one loop
+        for (int i = 1; i < n; ++i) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+            suffixSum[n - i - 1] = suffixSum[n - i] + nums[n - i - 1];
+        }
+        
+        // Calculate absolute differences and update the result array
+        for (int i = 0; i < n; ++i) {
+            int currentAbsoluteDiff = ((nums[i] * i) - prefixSum[i]) + (suffixSum[i] - (nums[i] * (n - i - 1)));
+            result[i] = currentAbsoluteDiff;
+        }
+        
+        return result;
+    }
+};
