b74bc2bd2888118708901890dbe4e3167e8a96ac
Shashwat Khare <97393095+Skarn7124@users.noreply.github.com>
2024-09-07 22:16:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        int ans = 0,t;
+        for(auto it : s){
+            t = it-'a'+1;
+            while(t){
+                ans += t%10;
+                t /= 10;
+            }
+        }
+        if(k == 1)
+            return ans;
+        while(k--){
+            int t = 0;
+            while(ans){
+                t += ans%10;
+                ans /= 10;
+            }
+            ans = t;
+        }
+        return ans;
+    }
+};
