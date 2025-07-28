6e6c00c9489b9606a0710985f49a683c3a0e4fc6
Shashwat Khare <97393095+Skarn7124@users.noreply.github.com>
2025-07-28 21:55:18 +0530

    Time: 122 ms (16.79%), Space: 310.1 MB (11.47%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int gen(int i,int n,int cur,int max,vector<int>& nums){
+    int gen(int i,int n,int cur,int max,vector<int>& nums,vector<vector<int>>&dp){
         if(i == n){
             if(cur == max)
                 return 1;
             return 0;
         }
-        int ans = gen(i+1,n,cur|nums[i],max,nums)+gen(i+1,n,cur,max,nums);
-        return ans;
+        if(dp[i][cur] != -1)
+            return dp[i][cur];
+        dp[i][cur] = gen(i+1,n,cur|nums[i],max,nums,dp)+gen(i+1,n,cur,max,nums,dp);
+        return dp[i][cur];
     }
     int countMaxOrSubsets(vector<int>& nums) {
         int max = 0;
@@ -15,7 +17,8 @@ public:
         for(int i = 0;i<n;i++){
             max |= nums[i];
         }
-        int ans = gen(0,n,0,max,nums);
+        vector<vector<int>> dp(n,vector<int>(max+1,-1));
+        int ans = gen(0,n,0,max,nums,dp);
         return ans;
     }
 };
